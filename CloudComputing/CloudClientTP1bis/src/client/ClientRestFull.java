a1994919a67edcf72017d75248f1317d236a688d
syescassut <Sylvain.ESCASSUT@etu.uca.fr>
2018-02-01 14:15:50 +0100

    fix typo

--- CloudComputing/CloudClientTP1bis/src/client/ClientRestFull.java
+++ CloudComputing/CloudClientTP1bis/src/client/ClientRestFull.java
@@ -5,6 +5,10 @@
  */
 package client;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
 /**
  * Jersey REST client generated for REST resource:GenericResource [generic]<br>
  * USAGE:
@@ -18,5 +22,27 @@ package client;
  * @author syescassut1
  */
 public class ClientRestFull {
-    
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/CloudTP1bis/webresources";
+
+    public ClientRestFull() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("generic");
+    }
+
+    public <T> T getXml(Class<T> responseType) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        resource = resource.path("get");
+        return resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(responseType);
+    }
+
+    public <T> T postXml(Class<T> responseType, String title) throws ClientErrorException {
+        return webTarget.path(java.text.MessageFormat.format("post/{0}", new Object[]{title})).request().post(null, responseType);
+    }
+
+    public void close() {
+        client.close();
+    }
 }
